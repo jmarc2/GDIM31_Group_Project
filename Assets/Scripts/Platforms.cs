a709e21209d2db54a1f92b5d26b3569dd6f507e5
Laura <lauruyen@gmail.com>
2023-11-30 16:49:15 -0800

    ch

--- Assets/Scripts/Platforms.cs
+++ Assets/Scripts/Platforms.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Platforms : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        //comment
+        
     }
 
     // Update is called once per frame
@@ -15,4 +16,5 @@ public class Platforms : MonoBehaviour
     {
         
     }
+
 }
