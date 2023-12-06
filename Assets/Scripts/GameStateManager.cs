0bbf516de69112174c6a9628ba0ba19eae158c93
Laura <lauruyen@gmail.com>
2023-12-05 20:32:35 -0800

    attempt

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static Action OnGameOver;
+    private static GameStateManager _instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(_instance);
+        }
+
+        else
+            if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void GameOver()
+    {
+        OnGameOver.Invoke();
+        Time.timeScale = 0;
+
+    }
+
+    public static void Restart()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+}
